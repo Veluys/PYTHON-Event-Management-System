32d0d2cd2db0af5a9ca9569926a8b3069196ac43
Vel <meisterrabyte@gmail.com>
2025-11-27 10:24:49 +0800

    Renamed DISPLAY_WIDTH to _DISPLAY_WIDTH

--- ems/view/displayer.py
+++ ems/view/displayer.py
@@ -1,9 +1,9 @@
-DISPLAY_WIDTH = 150
+_DISPLAY_WIDTH = 150
 
 def _display_header(header, upper_left, upper_right, middle, center, lower_left, lower_right):
-    upper_border = f"{upper_left}" + str(middle) * (DISPLAY_WIDTH - 2) + f"{upper_right}"
-    lower_border = f"{lower_left}" + str(middle) * (DISPLAY_WIDTH - 2) + f"{lower_right}"
-    centered_header = f"{center}" + str(header).center(DISPLAY_WIDTH - 2) + f"{center}"
+    upper_border = f"{upper_left}" + str(middle) * (_DISPLAY_WIDTH - 2) + f"{upper_right}"
+    lower_border = f"{lower_left}" + str(middle) * (_DISPLAY_WIDTH - 2) + f"{lower_right}"
+    centered_header = f"{center}" + str(header).center(_DISPLAY_WIDTH - 2) + f"{center}"
 
     print(upper_border)
     print(centered_header)
