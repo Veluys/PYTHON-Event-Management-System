9c2ce7bb668c976070c3836997ba100e3e946e22
Vel <meisterrabyte@gmail.com>
2025-11-27 14:27:20 +0800

    Add VenueDao class

---
+++ ems/model/venue_dao.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+import ems.model.dbconnection as dbconn
+import ems.model.venue as venue_model
+import ems.view.displayer as displayer
+
+class VenueDAO:
+    def __init__(self):
+        try:
+            self.conn = dbconn.create_connection()
+            self.cur = self.conn.cursor()
+        except Exception as err:
+            print("Database connection can't be established! Program would be terminated.")
+            displayer.show_error(err)
+            exit(1)
+
+    @staticmethod
+    def _is_a_venue(self, obj):
+        if not isinstance(obj, venue_model.Venue):
+            print("The passed parameter is not of venue type!")
+            return False
+        return True
+
+    def getVenueNames(self):
+        select_query = """
+            SELECT venue_name
+            FROM venues
+            ORDER BY venue_name
+        """
+
+        self.cur.execute(select_query)
+        return [row[0] for row in self.cur.fetchall()]
+
+venue_dao = VenueDAO()
