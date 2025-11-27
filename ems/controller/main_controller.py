9cb58b4315378aac06ee9185f265678dad8c5e24
Vel <meisterrabyte@gmail.com>
2025-11-27 10:48:25 +0800

    Add the start up page

--- ems/controller/main_controller.py
+++ ems/controller/main_controller.py
@@ -1,4 +1,18 @@
 import ems.view.displayer as displayer
+import ems.controller.input_getter as get_input
 
 def execute():
     displayer.display_header("Welcome to Event Management System!")
+
+    while True:
+        displayer.display_header("Start Page")
+
+        displayer.display_subheader("Main Menu")
+        main_menu_options = ("Events", "Registration", "Attendance", "Exit")
+        displayer.display_menu("What do you want to do or work with today?", main_menu_options)
+        option = get_input.getInt(len(main_menu_options))
+
+        match option:
+            case 4:
+                displayer.display_subheader("Thank you for using the system!")
+                return
