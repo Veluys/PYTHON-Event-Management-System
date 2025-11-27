810918d3fbebd9f399491005f80a87e99b74b878
Vel <meisterrabyte@gmail.com>
2025-11-27 10:35:35 +0800

    Add getInt

---
+++ ems/controller/input_getter.py
@@ -0,0 +1,17 @@
+import ems.view.displayer as displayer
+
+def getInt(highest, allow_blank=False):
+    user_input = input("Enter the number of your option: ")
+
+    if allow_blank and not user_input.strip():
+        return -1
+
+    try:
+        num_input = int(user_input)
+        if num_input < 1 or num_input > highest:
+            raise ValueError(f"Input invalid! Only positive integers between 1 and {highest} are allowed")
+    except ValueError as err:
+        displayer.show_error(err)
+        return -1
+    else:
+        return num_input
