408846c12150e070cc2252393d46b2510657ab36
Vel <meisterrabyte@gmail.com>
2025-11-27 18:08:44 +0800

    Add displayTable

--- ems/view/displayer.py
+++ ems/view/displayer.py
@@ -22,3 +22,28 @@ def display_menu(guide_msg, options):
 
 def show_error(error):
     print(f"Error: {type(error).__name__}, Message: {str(error)}\n")
+
+def displayTable(column_headers, records, column_sizes):
+    table_width = _DISPLAY_WIDTH - len(column_headers)
+
+    column_widths = [int(table_width * size) for size in column_sizes[:-1]]
+    column_widths.append(table_width - sum(column_widths))
+
+    def center_text(text, width, is_first):
+        total_padding = width - len(text)
+        if is_first:
+            total_padding -= 1
+        return str(text).center(total_padding)
+
+    def print_row(cells, column_widths):
+        print("|", end="")
+        for i, cell in enumerate(cells):
+            centered = center_text(cell, column_widths[i], i == 0)
+            print(centered + "|", end="")
+        print("\n" + "-" * _DISPLAY_WIDTH)
+
+    print_row(column_headers, column_widths)
+    for record in records:
+        print_row(record, column_widths)
+
+    print()
