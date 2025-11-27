91ca833c4f35d566bf1eb1d0fd0dc67ad2c115a6
Vel <meisterrabyte@gmail.com>
2025-11-27 10:47:41 +0800

    Add a newline after printing error

--- ems/view/displayer.py
+++ ems/view/displayer.py
@@ -15,10 +15,10 @@ def display_header(header):
 def display_subheader(header):
     _display_header(header, '+', '+', '-', '|', '+', '+')
 
-def show_menu(guide_msg, options):
+def display_menu(guide_msg, options):
     print(guide_msg)
     for i, option in enumerate(options, start=1):
         print(f"\t[{i}] {option}")
 
 def show_error(error):
-    print(f"Error: {type(error).__name__}, Message: {str(error)}")
+    print(f"Error: {type(error).__name__}, Message: {str(error)}\n")
