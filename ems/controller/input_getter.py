3fd55351d13d410cc67014e2d284945d83046b63
Vel <meisterrabyte@gmail.com>
2025-11-27 10:48:05 +0800

    Enclose previous getInt block inside a loop

--- ems/controller/input_getter.py
+++ ems/controller/input_getter.py
@@ -1,17 +1,18 @@
 import ems.view.displayer as displayer
 
 def getInt(highest, allow_blank=False):
-    user_input = input("Enter the number of your option: ")
+    while True:
+        user_input = input("Enter the number of your option: ")
 
-    if allow_blank and not user_input.strip():
-        return -1
+        if allow_blank and not user_input.strip():
+            return -1
 
-    try:
-        num_input = int(user_input)
-        if num_input < 1 or num_input > highest:
-            raise ValueError(f"Input invalid! Only positive integers between 1 and {highest} are allowed")
-    except ValueError as err:
-        displayer.show_error(err)
-        return -1
-    else:
-        return num_input
+        try:
+            num_input = int(user_input)
+            if num_input < 1 or num_input > highest:
+                raise ValueError(f"Input invalid! Only positive integers between 1 and {highest} are allowed")
+        except ValueError as err:
+            displayer.show_error(err)
+        else:
+            print()
+            return num_input
