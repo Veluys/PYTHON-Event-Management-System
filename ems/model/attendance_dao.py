0734221fafc1cc5191b38cdc8d687997c9d108f6
Vel <meisterrabyte@gmail.com>
2025-11-28 16:17:55 +0800

    Add view_attendance and its utility method _get_base_view_query

---
+++ ems/model/attendance_dao.py
@@ -0,0 +1,52 @@
+import psycopg2
+
+import ems.model.dbconnection as dbconn
+import ems.model.student as student_model
+import ems.view.displayer as displayer
+
+class AttendDao:
+    def __init__(self):
+        try:
+            self.conn = dbconn.create_connection()
+            self.cur = self.conn.cursor()
+        except Exception as err:
+            print("Database connection can't be established! Program would be terminated.")
+            displayer.show_error(err)
+            exit(1)
+
+    def _get_base_view_query(self):
+        return """
+            SELECT
+                s.sr_code,
+                pshortname,
+                year_level,
+                CONCAT(last_name, ', ', first_name, ' ', middle_name) AS full_name,
+                CASE
+                    WHEN attended = true
+                        THEN 'Yes'
+                    ELSE
+                        'No'
+                END AS attended
+            FROM students AS s
+            INNER JOIN registration AS r
+                ON s.sr_code = r.sr_code
+            INNER JOIN programs AS p
+                ON s.program_id = p.program_id
+            WHERE event_id = %s    
+        """
+
+    def view_attendance(self, event_id, attended):
+        view_query = self._get_base_view_query()
+        if attended is not None:
+            if attended:
+                view_query += " AND attended = true "
+            else:
+                view_query += " AND attended = false "
+
+            view_query += " ORDER BY year_level, pshortname, full_name; "
+
+        self.cur.execute(view_query, (event_id,))
+        participants = self.cur.fetchall()
+        return participants
+
+att_dao = AttendDao()
