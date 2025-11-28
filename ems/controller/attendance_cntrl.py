09ca1ba3e6b681cc9030325a4e7487da2f655b5e
Vel <meisterrabyte@gmail.com>
2025-11-28 16:18:10 +0800

    Add _view_attendance

--- ems/controller/attendance_cntrl.py
+++ ems/controller/attendance_cntrl.py
@@ -4,6 +4,7 @@ from ems.controller.input_getter import getLine
 from ems.model.registration_dao import reg_dao
 from ems.model.event_dao import event_dao
 from ems.model.student_dao import stud_dao
+from ems.model.attendance_dao import att_dao
 
 _VIEW_COLUMN_HEADERS = ("Sr-Code", "Program", "Year Level", "Full Name", "Attended")
 _VIEW_COLUMN_SIZES = (0.10, 0.20, 0.10, 0.50, 0.10)
@@ -37,5 +38,25 @@ def execute():
         option = get_input.getInt(len(att_operations))
 
         match option:
+            case 1:
+                _view_attendance(_selected_event_id, attended=True)
+            case 2:
+                _view_attendance(_selected_event_id, attended=False)
             case 6:
                 return
+
+def _view_attendance(event_id, attended):
+    participant_status = "Attendees" if attended else "Absentees"
+    displayer.display_subheader(f"Viewing {participant_status}")
+
+    try:
+        participants = att_dao.view_attendance(event_id, attended)
+    except Exception as err:
+        print(f"Fetching records of {participant_status} failed!")
+        displayer.show_error(err)
+    else:
+        if len(participants) == 0:
+            print(f"There are currently no {participant_status}!\n")
+            return
+        else:
+            displayer.displayTable(participant_status, _VIEW_COLUMN_HEADERS, participants, _VIEW_COLUMN_SIZES)
