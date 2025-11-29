a44298becf95b4e3bac21fd38e4079f6bfafcce2
Vel <meisterrabyte@gmail.com>
2025-11-29 10:58:59 +0800

    Add _update_attendance

--- ems/controller/attendance_cntrl.py
+++ ems/controller/attendance_cntrl.py
@@ -1,9 +1,7 @@
 import ems.view.displayer as displayer
 import ems.controller.input_getter as get_input
 from ems.controller.input_getter import getLine
-from ems.model.registration_dao import reg_dao
 from ems.model.event_dao import event_dao
-from ems.model.student_dao import stud_dao
 from ems.model.attendance_dao import att_dao
 
 _VIEW_COLUMN_HEADERS = ("Sr-Code", "Program", "Year Level", "Full Name", "Attended")
@@ -44,6 +42,10 @@ def execute():
                 _view_attendance(_selected_event_id, attended=False)
             case 3:
                 _search_registered(_selected_event_id)
+            case 4:
+                _update_attendance(_selected_event_id, setPresent=True)
+            case 5:
+                _update_attendance(_selected_event_id, setPresent=False)
             case 6:
                 return
 
@@ -79,3 +81,40 @@ def _search_registered(event_id):
             return
         else:
             displayer.displayTable("Matched Participant", _VIEW_COLUMN_HEADERS, participants, _VIEW_COLUMN_SIZES)
+
+def _update_attendance(event_id, setPresent):
+    if setPresent:
+        displayer.display_subheader("Marking Participant as Present")
+    else:
+        displayer.display_subheader("Marking Participant as Absent")
+
+    sr_code = get_input.getLine("Sr-Code: ")
+
+    if _can_update(event_id, sr_code, setPresent):
+        try:
+            att_dao.update_attendance(event_id, sr_code, setPresent)
+        except Exception as err:
+            print(f"Updating attendance status of {sr_code} failed!")
+            displayer.show_error(err)
+        else:
+            str_new_att_status = "Attendee" if setPresent else "Absentee"
+            print(f"The participant with an Sr Code of {sr_code} was successfully marked as an {str_new_att_status}.\n")
+
+def _can_update(event_id, sr_code, setPresent):
+    try:
+        curr_att_status = att_dao.is_attendee(event_id, sr_code)
+    except Exception as err:
+        print(f"Fetching records of {sr_code} failed!\n")
+        displayer.show_error(err)
+        return False
+
+    if curr_att_status is None:
+        print(f"There are no participants with an Sr-Code '{sr_code}'\n")
+        return False
+
+    if curr_att_status and setPresent:
+        str_curr_att_status = "Attendee" if curr_att_status else "Absentee"
+        print(f"The participant with an Sr-Code of {sr_code} is already marked as an {str_curr_att_status}\n")
+        return False
+
+    return True
